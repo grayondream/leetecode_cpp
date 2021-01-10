754dc760b0f050afd672fbd128e71947569686d4
grayondream <grayondream@outlook.com>
2021-01-10 16:14:59 +0800

    添加了有序数组中寻找中位数的代码注释

--- problems/1_100/004_find_median_sorted_array.hpp
+++ problems/1_100/004_find_median_sorted_array.hpp
@@ -51,7 +51,7 @@ double find_media_bipointer(vector<int> &nums1, vector<int> &nums2){
  * @brief 中位数是将一个数列划分为两半，左半部份的所有元素小于等于有半部分的所有元素，二分法首相从中间进行二分，然后在此的基础上不断逼近最终的可靠结果，拆分时因为数组是有序的，nums1[0 ~ i-1]和nums2[0 ~ j-1]是右=左半部分，nums2[j ~ len2)和nums1[i ~ len1)是右半部份，当左半部份的最大值小于有半部分的最小值时
  */
 double find_media_bin(vector<int> &nums1, vector<int> &nums2){
-    if(nums1.size() > nums2.size())             //保证第一个数组比第二个数组size小
+    if(nums1.size() > nums2.size())             //保证第一个数组比第二个数组size小,因为计算j时使用了(len1+len2+1)/2 - i，当i取len1时如果第一个数组大可能溢出
         return find_media_bin(nums2, nums1);
 
     int len1 = nums1.size();
@@ -69,7 +69,7 @@ double find_media_bin(vector<int> &nums1, vector<int> &nums2){
         int nums2_j_1 = (j == 0 ? INT32_MIN : nums2[j - 1]);
         int nums1_i   = (i == len1 ? INT32_MAX : nums1[i]);
         int nums2_j   = (j == len2 ? INT32_MAX : nums2[j]);
-        if(nums1_i_1 <= nums2_j){           //如果nums1_i_1 <= nums2_j能够保证左右两边都满足左边的元素都小于右边
+        if(nums1_i_1 <= nums2_j){           //如果nums1_i_1 <= nums2_j能够保证左右两边都满足左边的元素都小于右边，因为完整条件为nums1_i_1<=nums2_j && nums2_j_1 <= nums1_i，当第一个条件满足时第二个条件一定满足，因为i是不断增大的，而j是不断减小的
             med1 = nums1_i_1 > nums2_j_1 ? nums1_i_1 : nums2_j_1;           //左侧最大值
             med2 = nums1_i < nums2_j ? nums1_i : nums2_j;                   //右侧最小值
             left = i + 1;
