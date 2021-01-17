1215f5b5f2cbf2defa78840199b9fdb9d8192421
grayondream <grayondream@outlook.com>
2021-01-17 20:52:41 +0800

    昨天的每日一题有问题更正

--- problems/801_900/803_bricks_falling_when_hit.hpp
+++ problems/801_900/803_bricks_falling_when_hit.hpp
@@ -1,37 +1,39 @@
 #include "ios_include.hpp"
 
-/*
- * @brief 顶部的砖块和标记为2的砖块是稳定砖块
- */
-bool is_stable(vector<vector<int>> &grid, const int &x, const int &y) {
-    int row = grid.size();
-    int col = grid[0].size();
-    if (x == 0)
-        return true;
+class uset {
+public:
+    uset(int n) : parent(n), rank(n) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
 
-    if (x + 1 < row && grid[x + 1][y] == 2)
-        return true;
-    if (x - 1 >= 0 && grid[x - 1][y] == 2)
-        return true;
-    if (y + 1 < col && grid[x][y + 1] == 2)
-        return true;
-    if (y - 1 >= 0 && grid[x][y - 1] == 2)
-        return true;
+    int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
 
-    return false;
-}
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
 
-int dfs(vector<vector<int>> &grid, int x, int y) {
-    int row = grid.size();
-    int col = grid[0].size();
-    if (x < 0 || y < 0 || x >= row || x >= col || grid[x][y] != 1)
-        return 0;
+    void merge(int node1, int node2) {
+        int parent1 = find(node1);
+        int parent2 = find(node2);
+        if (parent1 == parent2)
+            return;
 
-    int ret = 0;
-    grid[x][y] = 2;
-    ret = 1 + dfs(grid, x + 1, y) + dfs(grid, x - 1, y) + dfs(grid, x, y - 1) + dfs(grid, x, y + 1);
-    return ret;
-}
+        parent[parent1] = parent2;
+        rank[parent2] += rank[parent1];
+    }
+
+    int size(int x) { return rank[find(x)]; }
+
+private:
+    vector<int> parent;
+    vector<int> rank;
+};
 
 /*
  * @brief 有一个 m x n 的二元网格，其中 1 表示砖块，0 表示空白。砖块 稳定（不会掉落）的前提是:
@@ -43,23 +45,62 @@ vector<int> hitBricks(vector<vector<int>> &grid, vector<vector<int>> &hits) {
     int row = grid.size();
     int col = grid[0].size();
     int len = hits.size();
-    for (auto ele : hits) {
-        grid[ele[0]][ele[1]] -= 1;
+    uset us(row * col + 1);
+    vector<vector<int>> status = grid;
+    //将打掉的砖块的状态转换成打掉之后的状态
+    for (int i = 0; i < len; i++) {
+        status[hits[i][0]][hits[i][1]] = 0;
     }
 
-    for (int i = 0; i < col; i++) {
-        dfs(grid, 0, i);
-    }
+    //遍历每一个结点创建打掉砖块之后的并查集
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (1 == status[i][j]) {
+                if (0 == i) { //顶部的节点是稳定的直接和col * row连接，col*row表示稳定点，每个点编码为i * col + j
+                    us.merge(col * row, i * col + j);
+                }
+                //将当前节点和上侧节点进行加入到并查集中同一个连通区域内
+                if (i > 0 && status[i - 1][j] == 1) {
+                    us.merge(i * col + j, (i - 1) * col + j);
+                }
 
-    vector<int> ret = vector<int>(len, -1);
-    for (int i = len - 1; i >= 0; i--) {
-        int x = hits[i][0], y = hits[i][1];
-        grid[x][y] += 1;
-        if (!is_stable(grid, x, y) || 0 == grid[x][y]) {
-            ret[i] = 0;
-        } else {
-            ret[i] = dfs(grid, x, y) - 1;
+                //将当前节点和左侧节点加入到并查集的同一个连通区域
+                if (j > 0 && status[i][j - 1] == 1) {
+                    us.merge(i * col + j, i * col + j - 1);
+                }
+            }
         }
+    }
+
+    const vector<pair<int, int>> dirs{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    vector<int> ret(len, 0);
+    for (int i = len - 1; i >= 0; i--) {
+        int x = hits[i][0];
+        int y = hits[i][1];
+        if (grid[x][y] == 0) { //节点没有砖块
+            continue;
+        }
+
+        //当前稳定的砖块数量
+        int pre = us.size(col * row);
+        if (x == 0) {
+            us.merge(y, row * col);
+        }
+
+        //遍历右，下，左，上四个方向的结点，反向添加砖块，将当前砖块周围存在的砖块连接
+        for (const auto dir : dirs) {
+            int cx = x + dir.first;
+            int cy = y + dir.second;
+            if (cx >= 0 && cx < row && cy >= 0 && cy < col) {
+                if (status[cx][cy] == 1) {
+                    us.merge(x * col + y, cx * col + cy);
+                }
+            }
+        }
+
+        int sz = us.size(col * row);
+        ret[i] = max(0, sz - pre - 1);
+        status[x][y] = 1;
     }
 
     return ret;
@@ -68,6 +109,8 @@ vector<int> hitBricks(vector<vector<int>> &grid, vector<vector<int>> &hits) {
 void test_803() {
     vector<vector<int>> grid = {{1}, {1}, {1}, {1}, {1}};
     vector<vector<int>> hits = {{3, 0}, {4, 0}, {1, 0}, {2, 0}, {0, 0}};
+    cout << grid.size() << grid[0].size() << endl;
+    cout << grid << endl;
     vector<int> ret = hitBricks(grid, hits);
     cout << ret << endl;
 }
--- utils/stream_tool.hpp
+++ utils/stream_tool.hpp
@@ -1,11 +1,12 @@
 #ifndef STREAM_TOOL_HPP
 #define STREAM_TOOL_HPP
 
-#include <ostream>
 #include <iostream>
+#include <ostream>
 #include <vector>
 
 using std::cout;
+using std::endl;
 using std::ostream;
 using std::vector;
 
@@ -13,12 +14,24 @@ using std::vector;
  * @brief 容器常用的和标准输出相交互的工具函数
  */
 
-template<typename T>
-ostream& operator<<(ostream& os, vector<T> &vec){
-    for(auto val : vec){
-        cout<<val<<"\t";
+template <typename T> ostream &operator<<(ostream &os, vector<T> &vec) {
+    for (auto val : vec) {
+        cout << val << "\t";
     }
 
     return os;
 }
+
+template <typename T> ostream &operator<<(ostream &os, vector<vector<T>> &vec) {
+    for (auto v : vec) {
+        for (auto e : v) {
+            os << e << " ";
+        }
+
+        os << endl;
+    }
+
+    return os;
+}
+
 #endif
